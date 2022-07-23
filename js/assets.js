43c30de8a2237c5224edb0e482203b1f3e80ad98
Vince McKeown <vmckeown79@gmail.com>
2022-07-23 11:00:07 -0400

    Aligned funguy

--- js/assets.js
+++ js/assets.js
@@ -527,6 +527,6 @@ let miniaAssets = [
     Sprite.xspec({tag: 'hud.gbutton.pressed', img: new SheetRef({src: 'img/hud.png', width: 32, height: 32, x: 16*14, y: 16*2})}),
     Sprite.xspec({tag: 'hud.gbutton.highlight', img: new SheetRef({src: 'img/hud.png', width: 32, height: 32, x: 16*14, y: 16*4})}),
 
-    Sprite.xspec({tag: 'funguy', img: new SheetRef({src: 'img/funguy.png', width: 16, height: 24, x: 16*1, y: 16*0})}),
+    Sprite.xspec({tag: 'funguy', img: new SheetRef({src: 'img/funguy.png', width: 16, height: 32, x: 16*0, y: 16*0})}),
 
 ];
