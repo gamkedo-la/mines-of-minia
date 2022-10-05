a4cfb8dad2715a8107a67d2273ea283c34f22482
Tylor Allison <tylor@foobox.com>
2022-10-05 08:15:10 -0500

    example item names

---
+++ js/procgen/names.js
@@ -0,0 +1,53 @@
+export { Names };
+
+/**
+ * Names to randomly assign to the different types of items within game
+ */
+class Names {
+
+    static bonk = [
+        'buncha crunch',
+        'knoggin knocker',
+    ];
+
+    static poke = [
+        'needler',
+        'stabby mcstabby',
+    ];
+
+    static hack = [
+        'here\'s johnny',
+        'chippy',
+    ];
+
+    static ice = [
+        'ice ice baby',
+    ];
+
+    static shock = [
+        'live wire',
+    ];
+
+    static fire = [
+        'firestarter',
+    ];
+
+    static dark = [
+        'like my soul',
+        'fear the reaper',
+    ];
+
+    static reactor = [
+        'gerbilator',
+        'fusiamatic',
+    ];
+
+    static shield = [
+        'bit-o-foil',
+    ];
+
+    static gadget = [
+        'thingamajig',
+        'whatchamacallit',
+    ];
+}
