db57859a9bcfd0926b85d16a0d9383fc983f9540
Manuel Albarracin <roxcon909@gmail.com>
2022-10-06 15:48:49 -0300

    Update names.js
    added names to all weapon types except Dark

--- js/procgen/names.js
+++ js/procgen/names.js
@@ -8,28 +8,44 @@ class Names {
     static bonk = [
         'buncha crunch',
         'knoggin knocker',
+        'winemaker',
+        'diamond cracker',
+        'world flattener',
     ];
 
     static poke = [
         'needler',
         'stabby mcstabby',
+        'knight\'s toothpick',
+        'war needles',
+        'slightly sharpened stick',
     ];
 
     static hack = [
         'here\'s johnny',
         'chippy',
+        'peasant\'s garden tool',
+        'fleshy-tree cutter',
+        'landscaper\'s partner ',
+
     ];
 
     static ice = [
         'ice ice baby',
+        'ice-cream supplier',
+        'summer\'s foe',
     ];
 
     static shock = [
         'live wire',
+        'bill raiser',
+        'zeus\' little spark',
     ];
 
     static fire = [
         'firestarter',
+        'habanero server',
+        'carbonizer',
     ];
 
     static dark = [
@@ -40,14 +56,18 @@ class Names {
     static reactor = [
         'gerbilator',
         'fusiamatic',
+        'electro-wind converter'
     ];
 
     static shield = [
         'bit-o-foil',
+        'reinforcement plate',
+        'conveniently shaped metal'
     ];
 
     static gadget = [
         'thingamajig',
         'whatchamacallit',
+        'jumbled mombo',
     ];
 }
