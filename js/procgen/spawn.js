a4a6419fe0d2c2e361927c0a9b6b9355da40d5d7
Tylor Allison <tylor@foobox.com>
2022-11-04 08:49:34 -0500

    add enemies back in

--- js/procgen/spawn.js
+++ js/procgen/spawn.js
@@ -66,7 +66,7 @@ class Spawn {
         // -- lock and key
         this.spawnLockAndKeys(template, pstate);
         // -- enemies
-        //this.spawnEnemies(template, pstate);
+        this.spawnEnemies(template, pstate);
         // -- traps
         this.spawnTraps(template, pstate);
         // -- growth
