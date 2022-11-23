dd3e591430a49614d0031011da2cc5fa8f161ce2
Tylor Allison <tylor@foobox.com>
2022-11-23 09:54:34 -0600

    wip: menu art layout/art integration

--- js/menuState.js
+++ js/menuState.js
@@ -21,15 +21,16 @@ import { Prompt } from './prompt.js';
 import { Resurrect64 } from './resurrect64.js';
 import { Serialization } from './serialization.js';
 import { Help } from './help.js';
+import { Assets } from './base/assets.js';
 
 function button(text, spec) {
     return Object.assign({}, UxButton.xspec({
         x_textXform: XForm.xspec({offset: 10}),
-        highlight: Sketch.zero,
-        unpressed: Sketch.zero,
-        pressed: Sketch.zero,
+        highlight: Assets.get('menu.button', true),
+        unpressed: Assets.get('menu.button', true),
+        pressed: Assets.get('menu.button', true),
         text: new Text({text: text, color: Resurrect64.colors[0]}),
-        hltext: new Text({text: text, color: Resurrect64.colors[10]}),
+        hltext: new Text({text: text, color: Resurrect64.colors[16]}),
     }), spec);
 }
 
@@ -92,6 +93,22 @@ class MenuState extends GameState {
         });
     }
 
+    onTock(evt) {
+        let cogRate1 = .001;
+        let cogRate2 = -.002;
+        let dt = evt.deltaTime;
+        // cog 1
+        let angle = this.cog1.xform.angle + cogRate1*dt;
+        if (angle > Math.PI*2) angle -= Math.PI*2;
+        this.cog1.xform.angle = angle;
+        this.cog1.evt.trigger(this.cog1.constructor.evtUpdated, { actor: this.cog1 });
+        // cog 2
+        angle = this.cog2.xform.angle + cogRate2*dt;
+        if (angle < -Math.PI*2) angle += Math.PI*2;
+        this.cog2.xform.angle = angle;
+        this.cog2.evt.trigger(this.cog1.constructor.evtUpdated, { actor: this.cog2 });
+    }
+
     async ready() {
         let x_view = UxCanvas.xspec({
             cvsid: 'game.canvas',
@@ -101,17 +118,28 @@ class MenuState extends GameState {
                 UxPanel.xspec({
                     sketch: Sketch.zero,
                 }),
+                // -- cogs
+                UxPanel.xspec({
+                    tag: 'cog1',
+                    sketch: Assets.get('cog1', true),
+                    x_xform: XForm.xspec({left: 1/39, right: 32/39, top: 8/21, bottom: 7/21, width: 6, height: 6, lockRatio: true}),
+                }),
+                UxPanel.xspec({
+                    tag: 'cog2',
+                    sketch: Assets.get('cog6', true),
+                    x_xform: XForm.xspec({left: 0/39, right: 35/39, top: 5/21, bottom: 12/21, width: 4, height: 4, lockRatio: true}),
+                }),
                 // -- button panel
                 UxPanel.xspec({
                     tag: 'menu.panel',
-                    sketch: Sketch.zero,
-                    x_xform: XForm.xspec({top: .2, bottom: .2, left: .3, right: .3}),
+                    sketch: Assets.get('menu.bg', true),
+                    x_xform: XForm.xspec({left: 14/39, right: 13/39, top: 3/21, bottom: 2/21, left: .3, width: 12, height: 16, lockRatio: true}),
                     x_children: [
-                        button('   new   ', { tag: 'menu.new', x_xform: XForm.xspec({top: 0, bottom: .8}), }),
-                        button('   load   ', { tag: 'menu.load', x_xform: XForm.xspec({top: .2, bottom: .6}), }),
-                        button('   options   ', { tag: 'menu.options', x_xform: XForm.xspec({top: .4, bottom: .4}), }),
-                        button('   help   ', { tag: 'menu.help', x_xform: XForm.xspec({top: .6, bottom: .2}), }),
-                        button('   credits   ', { tag: 'menu.credits', x_xform: XForm.xspec({top: .8, bottom: 0}), }),
+                        button('     new     ', { tag: 'menu.new', x_xform: XForm.xspec({left: 2/12, right: 4/12, top: 1/16, bottom: 13/16}), }),
+                        button('     load     ', { tag: 'menu.load', x_xform: XForm.xspec({left: 2/12, right: 4/12, top: 4/16, bottom: 10/16}), }),
+                        button('  options  ', { tag: 'menu.options', x_xform: XForm.xspec({left: 2/12, right: 4/12, top: 7/16, bottom: 7/16}), }),
+                        button('     help     ', { tag: 'menu.help', x_xform: XForm.xspec({left: 2/12, right: 4/12, top: 10/16, bottom: 4/16}), }),
+                        button('  credits  ', { tag: 'menu.credits', x_xform: XForm.xspec({left: 2/12, right: 4/12, top: 13/16, bottom: 1/16}), }),
                     ],
                 }),
                 /*
@@ -130,6 +158,8 @@ class MenuState extends GameState {
         this.optionsButton = Hierarchy.find(this.view, (v) => v.tag === 'menu.options');
         this.helpButton = Hierarchy.find(this.view, (v) => v.tag === 'menu.help');
         this.creditsButton = Hierarchy.find(this.view, (v) => v.tag === 'menu.credits');
+        this.cog1 = Hierarchy.find(this.view, (v) => v.tag === 'cog1');
+        this.cog2 = Hierarchy.find(this.view, (v) => v.tag === 'cog2');
         // -- disable load if no game is saved...
         if (!Serialization.hasSaveGame()) {
             this.loadButton.active = false;
@@ -140,11 +170,13 @@ class MenuState extends GameState {
         this.onOptionsClicked = this.onOptionsClicked.bind(this);
         this.onHelpClicked = this.onHelpClicked.bind(this);
         this.onCreditsClicked = this.onCreditsClicked.bind(this);
+        this.onTock = this.onTock.bind(this);
         this.newButton.evt.listen(this.newButton.constructor.evtMouseClicked, this.onNewClicked);
         this.loadButton.evt.listen(this.loadButton.constructor.evtMouseClicked, this.onLoadClicked);
         this.optionsButton.evt.listen(this.optionsButton.constructor.evtMouseClicked, this.onOptionsClicked);
         this.helpButton.evt.listen(this.helpButton.constructor.evtMouseClicked, this.onHelpClicked);
         this.creditsButton.evt.listen(this.creditsButton.constructor.evtMouseClicked, this.onCreditsClicked);
+        Events.listen('game.tock', this.onTock);
         return Promise.resolve();
     }
 
