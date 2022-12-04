46bed9a2e8f97ca5a7a1064eefa77c3e7ceec2e6
Tylor Allison <tylor@foobox.com>
2022-12-03 21:09:10 -0600

    re-enable final boss fight

--- js/entities/overbearer.js
+++ js/entities/overbearer.js
@@ -80,31 +80,25 @@ class Overbearer extends Enemy{
                 yield null;
                 break;
             case 'powerup.bull':
-                /*
                 console.log(`state is: ${this.state}`);
                 yield new PowerupBullAction({
                     lvl: this.elvl,
                 });
                 UpdateSystem.eUpdate(this, {state: 'inactive'});
                 break;
-                */
             case 'powerup.stealth':
-                /*
                 console.log(`-- starting power up stealth`);
                 yield new PowerupStealthAction({
                     lvl: this.elvl,
                 });
                 UpdateSystem.eUpdate(this, {state: 'inactive'});
                 break;
-                */
             case 'powerup.thump':
-                /*
                 yield new PowerupThumpAction({
                     lvl: this.elvl,
                 });
                 UpdateSystem.eUpdate(this, {state: 'inactive'});
                 break;
-                */
             case 'finale':
                 yield new FinaleAction({
                     lvl: this.elvl,
