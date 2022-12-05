6bcb4a8d24905069b8d42970aa068384b54cbc5b
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2022-12-05 16:06:21 +0100

    If localstorage is not available, fallback to temporary storage (persists as long as the tab is open)

--- js/base/storage.js
+++ js/base/storage.js
@@ -1,6 +1,7 @@
-export { Storage };
+// This file is designed to not crash if access to window.localstorage is not allowed.
+// See the end of this file for the exports (they need to be after the definitons)
 
-class Storage {
+class LocalStorage {
     static setItem(key, value) {
         let str = JSON.stringify(value);
         localStorage.setItem(key, str);
@@ -20,3 +21,48 @@ class Storage {
     }
 
 }
+
+class TemporaryStorage {
+
+    storage = {};
+
+    getItem(name){
+        try{
+            const item_str = storage[name];
+            if(typeof item_str === 'string'){
+                return JSON.parse(item_str);
+            }
+        } catch(error) {
+            console.warn(`failed to get value in storage: ${error}`);
+        }
+    }
+
+    setItem(name, item){
+        try{
+            const item_str = JSON.stringify(item);
+            storage[name] = item_str;
+        } catch(error){
+            console.warn(`failed to set value in storage: ${error}`);
+        }
+
+    }
+
+    removeItem(name){
+        delete storage[name];
+    }
+
+};
+
+function is_local_storage_available() {
+    try {
+        window.localStorage.setItem("check", { something: "ok" });
+        window.localStorage.removeItem("check");
+        return true;
+    } catch(error) {
+        return false;
+    }
+}
+
+export const isLocalstorageAllowed = is_local_storage_available();
+const Storage = isLocalstorageAllowed ? LocalStorage : TemporaryStorage;
+export { Storage };
